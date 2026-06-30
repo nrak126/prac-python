17bb9b061f5ef15f11142996ee1e968388c1c97a
nrak126 <daichidaichi126@gmail.com>
2026-06-30 18:40:46 +0900

    fix

--- 03/ex08_json/main.py
+++ 03/ex08_json/main.py
@@ -15,7 +15,7 @@ with open("exam_results.json", "r") as f:
     data = json.load(f)
 
 # TODO: calc_average を呼び出して平均点を表示する
-print(calc_average(data))
+print("avg: ", calc_average(data))
 
 # TODO: find_top_scorer を呼び出して最高得点者の名前と点数を表示する
-print(find_top_scorer(data))
+print("max: ", find_top_scorer(data))
